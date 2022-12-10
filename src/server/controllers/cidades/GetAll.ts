91a87b0b34b59cbbd99201eb91ff226a160decc3
Luis Felipe <lfelipeggarcia@gmail.com>
2022-12-09 22:19:25 -0300

    feat: controllers

--- src/server/controllers/cidades/GetAll.ts
+++ src/server/controllers/cidades/GetAll.ts
@@ -1,9 +1,11 @@
-import { Request, RequestHandler, Response } from 'express';
+import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import * as yup from 'yup';
+import { CidadesProvider } from '../../database/providers/cidades';
 import { validation } from '../../shared/middleware';
 
 interface IQueryProps {
+  id?: number;
   page?: number;
   limit?: number;
   filter?: string;
@@ -14,6 +16,7 @@ export const getAllValidation = validation((getSchema) => ({
     yup.object().shape({
       page: yup.number().notRequired().moreThan(0),
       limit: yup.number().notRequired().moreThan(0),
+      id: yup.number().integer().notRequired().default(0),
       filter: yup.string().notRequired(),
     })
   ),
@@ -23,13 +26,25 @@ export const getAll = async (
   req: Request<{}, {}, {}, IQueryProps>,
   res: Response
 ) => {
-  res.setHeader('acess-control-expose-headers', 'x-total-count');
-  res.setHeader('x-total-count', 1);
+  const result = await CidadesProvider.getAll(
+    req.query.page || 1,
+    req.query.limit || 7,
+    req.query.filter || '',
+    Number(req.query.id)
+  );
+  const count = await CidadesProvider.count(req.query.filter);
 
-  return res.status(StatusCodes.OK).json([
-    {
-      id: 1,
-      nome: 'Campinas',
-    },
-  ]);
+  if (result instanceof Error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      errors: { default: result.message },
+    });
+  } else if (count instanceof Error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      errors: { default: count.message },
+    });
+  }
+  res.setHeader('access-control-expose-headers', 'x-total-count');
+  res.setHeader('x-total-count', count);
+
+  return res.status(StatusCodes.OK).json(result);
 };
--- src/server/controllers/cidades/UpdateById.ts
+++ src/server/controllers/cidades/UpdateById.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import * as yup from 'yup';
 import { ICidade } from '../../database/models';
+import { CidadesProvider } from '../../database/providers/cidades';
 import { validation } from '../../shared/middleware';
 
 interface IParamProps {
@@ -26,12 +27,21 @@ export const updateById = async (
   req: Request<IParamProps, {}, IBodyProps>,
   res: Response
 ) => {
-  if (Number(req.params.id) === 99999)
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+  if (!req.params.id) {
+    return res.status(StatusCodes.BAD_REQUEST).json({
       errors: {
-        default: 'Registro não encontrado',
+        default: 'O parâmetro "id" precisa ser informado.',
       },
     });
+  }
 
-  return res.status(StatusCodes.NO_CONTENT).send();
+  const result = await CidadesProvider.updateById(req.params.id, req.body);
+  if (result instanceof Error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      errors: {
+        default: result.message,
+      },
+    });
+  }
+  return res.status(StatusCodes.NO_CONTENT).json(result);
 };
